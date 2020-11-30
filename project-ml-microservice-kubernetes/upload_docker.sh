03628ffa2a9fb5e6a51810f6c5488f35e132849b
almahaalhizam <alhizamalmaha@gmail.com>
2020-12-01 02:43:29 +0300

    Modified file with steps to upload image to Docker Hub

--- project-ml-microservice-kubernetes/upload_docker.sh
+++ project-ml-microservice-kubernetes/upload_docker.sh
@@ -6,10 +6,14 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath="almaha96/udacity"
 
 # Step 2:  
 # Authenticate & tag
+docker login --username "almaha96" &&\
+  docker image tag udacity $dockerpath
 echo "Docker ID and Image: $dockerpath"
 
 # Step 3:
 # Push image to a docker repository
+docker image push $dockerpath
